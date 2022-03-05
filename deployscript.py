59217174d770c06bc3124bdf1ba7bd0c0b2b4877
michaelescue <mescue@pdx.edu>
2022-03-04 17:19:47 -0800

    Fixed bug with argv.

--- deployscript.py
+++ deployscript.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     config = SshConfig
 
     # If Command Line args included, update config.
-    if sys.argv[1]:
+    if len(sys.argv)>1:
         # User
         config.user = sys.argv[1]
         # Host
