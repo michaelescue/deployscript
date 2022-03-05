3cb443e9e8564b97cc163e3597d848298a83ac70
michaelescue <mescue@pdx.edu>
2022-03-04 19:28:57 -0800

    Generalized authorization to password or key. Added error handling.

--- deployscript.py
+++ deployscript.py
@@ -4,14 +4,16 @@
 # Date 3/3/22
 # This is a  Python (3.8) script.
 #
-# Usage: python deployscript.py user host port password script
+# Usage: python deployscript.py user host port [key or password] script
 #
 # Test setup:
+#   Included Anaconda environment(Python 3.8)
 #   Windows 11 Home 22000.493 (local) machine
 #   Linux 5.13.0-30-generic (remote) machine
 #   - PasswordAuthentication Allowed
 #   - Port 254 instead of 22 (Personal configuration)
 #   SSH connections performed over LAN due to closed ports on ISP.
+#   SSH key in OpenSSH format
 #
 # Brief:
 #   Creates ssh connection with remote, transfers shell script to execute,
@@ -41,15 +43,13 @@ def runScript(client, c):
     stdin, stdout, stderr = client.exec_command(f'chmod +x ~/Desktop/{c.remote_script}')
 
     # Parse stdout/stderr responses
-    print(''.join(stdout.readlines()))
-    print(''.join(stderr.readlines()))
+    response(stdout, stderr)
 
     # Execute shell script over ssh
     stdin, stdout, stderr = client.exec_command(f'bash ~/Desktop/{c.remote_script}')
 
     # Parse stdout/stderr responses
-    print(stdout.readlines())
-    print(stderr.readlines())
+    response(stdout, stderr)
 
 def getData(client, c):
     print(f'Getting {c.remote_data} from {c.user}@{c.host}.\n')
@@ -60,12 +60,20 @@ def getData(client, c):
     # Transfer data to local machine
     sftp.get(f'{c.remote_script_dest}/{c.remote_data}', f'{c.cwd}/{c.remote_data}')
 
+def response(stdout,stderr):
+    line = ''.join(stdout.readlines())
+    if line:
+        print(line)
+    line = ''.join(stderr.readlines())
+    if line:
+        print(line)
+
 # Configuration class holding
 class SshConfig:
     user = 'server'
     host = '192.168.0.3'
     port = 254  # typically 22, but personal ssh server setup differently.
-    password = '********' # Enter password here, or on command line.
+    auth = 'C:/Users/ME/OneDrive/Documents/ssshk/mehpsshserver-openssh' # Enter password here, or on command line.
     cwd = os.getcwd()
     remote_script = 'remote_script.sh'
     remote_script_path = f'{cwd}/{remote_script}'
@@ -78,25 +86,52 @@ if __name__ == '__main__':
     config = SshConfig
 
     # If Command Line args included, update config.
-    if len(sys.argv)>1:
-        # User
-        config.user = sys.argv[1]
-        # Host
-        config.host = sys.argv[2]
-        # Port
-        config.port = sys.argv[3]
-        # Password
-        config.password = sys.argv[4]
-        # Script to deploy
-        config.script = sys.argv[5]
+    try:
+        if (len(sys.argv)>1):
+            # User
+            config.user = sys.argv[1]
+            # Host
+            config.host = sys.argv[2]
+            # Port
+            config.port = sys.argv[3]
+            # Password
+            config.auth = sys.argv[4]
+            # Script to deploy
+            config.script = sys.argv[5]
 
-    # SSH client
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(config.host,
-                config.port,
-                config.user,
-                config.password,)
+        # SSH client
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        # Connect with key or password
+        # key must be OpenSSH privat key format
+        if os.path.exists(config.auth) is True:
+            print("Connecting with key pair.")
+            ssh.connect(config.host,
+                        config.port,
+                        config.user,
+                        key_filename=config.auth
+                        )
+        else:
+            ssh.connect(config.host,
+                        config.port,
+                        config.user,
+                        password=config.auth
+                        )
+
+    except paramiko.ssh_exception.AuthenticationException:
+        for each in['Connectivity issue',
+                  'Things to check for:',
+                  '- Correct user.',
+                  '- Correct password.',
+                  '- Correct host.',
+                   '- Correct port.',
+                   'Exiting.'
+                  ]:
+            print(each)
+
+        exit()
+        pass
 
     # Required steps
     sendScript(ssh, config)
